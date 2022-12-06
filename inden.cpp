123be0a97be6fecc6a6e61db95d3eda03741aee2
ReemHassan578 <reemhassan5523@gmail.com>
2022-12-06 13:18:50 -1000

    formating function

--- inden.cpp
+++ inden.cpp
@@ -42,11 +42,38 @@ vector <string> readxml(string name)
         return contents;
     }
 
+
 vector <string> prettify(string name)
 { //vector<string> check;
- vector <string> tem=readxml( name);
-vector <string>::iterator it;
+   // string parent= "<users>";
+    //tring children1= "<user>";
+    //ing children2= "<name>";
+    //string children3= "<id>" ;
+   // string children4= "<posts>";
+    //string children5= "<post>" ;
+    //string children6= "<body>" ;
+    //string children7= "<topic>" ;
+    //string children8= "<topics>" ;
+   // string children9= "<followers>" ;
+    //string children10= "<follower>" ;
+std::stack<string> *ch;
+//stack *ch;
+ vector <string> tem=readxml(name);
+//vector<string>::iterator itr;
 for (int y = 0; y < tem.size();y++)
+{ int i ;
+
+if ((tem[y].find("<users>"))||(tem[y].find("</users>"))) {cout << tem[y] << endl;}
+if ((tem[y].find("<user>"))||(tem[y].find("</user>"))) {cout<< "\t"<<endl;cout << tem[y] << endl;}
+if ((tem[y].find("<id>"))||(tem[y].find("</id>"))|| (tem[y].find("<posts>"))||(tem[y].find("</posts>"))||(tem[y].find("<name>"))||(tem[y].find("</name>"))|| (tem[y].find("<followers>"))||(tem[y].find("</followers>"))) { if(tem[y].find("<id>")||(tem[y].find("</id>"))) { if(ch->top()=="<follower>") {ch->pop();cout<<"\t"<<"\t"<<"\t"<< "\t"<< endl;cout << tem[y] << endl;}} cout<< "\t"<<"\t"<<endl; cout <<tem[y]<<endl;}
+if ((tem[y].find("<post>"))||(tem[y].find("</post>"))||(tem[y].find("<follower>"))||(tem[y].find("</follower>",y))) { if(tem[y].find("<follower>",y)){ch->push("<follower>");cout<< "\t"<<"\t"<<"\t" <<endl;cout << tem[y] << endl;}}
+if ((tem[y].find("<body>"))||(tem[y].find("</body>"))|| (tem[y].find("<topics>"))||(tem[y].find("</topics>",y))) {cout<< "\t"<<"\t"<<"\t"<< "\t"<< endl;cout << tem[y] << endl;}
+if ((tem[y].find("<topic>"))||(tem[y].find("</topic>"))) {cout<< "\t"<<"\t"<<"\t"<< "\t"<<"\t"<<endl; cout << tem[y] << endl;}
+
+}
+}
+//vector <string>::iterator it;
+/*for (int y = 0; y < tem.size();y++)
 { it= find(tem.begin(),tem.end(),"</");
 if(it==tem.end())
 {cout<< "\t"<<endl;
@@ -54,8 +81,8 @@ if(it==tem.end())
 
    // int i= tem[y].find("<",y);
     //int j=tem[y].find(">",i+1);
-}}
-
+}} if(tem.find("<id>",y)||if(tem.find("</id>",y)) {if(ch->top()=="<follower>")  cout<< "\t"<<"\t"<<"\t"<< "\t"<< endl; cout <<tem[y]<<endl;}}}
+*/
 /*for (int y = 0; y < tem.size();)
    {  int i= tem[y].find("<",y);
        while (i!= tem[y].size())
