e2c2b4747ab56525f59d65bc2bc4251e2483daaa
zainbmaged <1901420@eng.asu.edu.eg>
2023-01-30 07:43:48 +0200

    Update Network_Analysis.cpp

--- Network_Analysis.cpp
+++ Network_Analysis.cpp
@@ -8,14 +8,15 @@
 #include <cctype>
 #include <cstring>
 #include <stack>
+#include<algorithm>
+
 #pragma warning(suppress : 4996)
 
 using namespace std;
 
-string str1;
-int nUsers;
 
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //function to read xml file line by line  and return it in one string in one string O(n)----> n is number of lines in xml file
 string readxml(string name)
 {
@@ -39,7 +40,7 @@ string readxml(string name)
     xmlfile.close();
     return contents;
 }
-
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //saving tags  in vector by processing xmlstring char by char O(n)----> n is number of chars in xml file
 vector <string> tags(string xmlstring) {
     vector <string> tag;
@@ -72,7 +73,8 @@ vector <string> tags(string xmlstring) {
 
     return tag;//returning tag vector
 }
-
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// class nodes 
 class Node {
 public:
     string id;
@@ -80,27 +82,28 @@ public:
     vector < string> postBody;
     vector < string> postTopic;
     vector<string> follower;
-    int sizes = 0;
+    int size = 0;
     Node(string id, string name) {
         this->id = id;
         this->name = name;
-       
+        size++;
     }
-   
+    
 };
 
 
-
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//function to construct user nodes and insert followers , posts and topics for each
 vector<Node*> toNodes(vector <string> tags) {
     vector<Node*> users;
     for (int i = 0; i < tags.size(); i++) {
-        if (tags[i] == "user"){
-            users.push_back(new Node(tags[i + 2],tags[i+5]));
-            int j = i+5;
-            while (tags[j] != "/user" && j+2<tags.size()) {
-                
+        if (tags[i] == "user") {
+            users.push_back(new Node(tags[i + 2], tags[i + 5]));
+            int j = i + 5;
+            while (tags[j] != "/user" && j + 2 < tags.size()) {
+
                 if (tags[j] == "post") {
-                    users[users.size()-1]->postBody.push_back(tags[j + 2]);
+                    users[users.size() - 1]->postBody.push_back(tags[j + 2]);
                 }
                 if (tags[j] == "follower") {
                     users[users.size() - 1]->follower.push_back(tags[j + 2]);
@@ -114,8 +117,83 @@ vector<Node*> toNodes(vector <string> tags) {
     }
     return users;
 }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Network Analysis Functions
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//who is the most influencer user (has the most followers)O(n*m) n is users number and m is followers of most infulencer
+string Influncer(vector<Node*> users) {
+    int max_followers=0;
+    string influncer;
+    for (Node* user : users) {
+        if (user->follower.size() > max_followers) {
+            influncer = user->name;
+            max_followers = user->follower.size();
+        }
+    }
+    return influncer;
+}
 
+//who is the most active user (connected to lots of users) O(n^2*m) n is number of users m is max followers of any user
+string Active(vector<Node*> users) {
+    int max_following = 0;
+    
+    string active;
+    int tem = 0;
+    for (Node* user : users) {
+        for (Node* useri : users) {
+            for (string follower : useri->follower) {
+                if (user->id == follower) {
+                    tem ++;
+                }
+            }
+
+        }
+        if (tem > max_following) {
+            max_following = tem;
+            tem = 0;
+            active = user->name;
+        }
+        else {
+            tem = 0;
+        }
+    }
+    
+    return active;
+}
+
+//the mutual followers between 2 users O(n*m) n is followers of 1st and m is followers of 2nd
+vector<string> common_followers(Node* user1, Node* user2, vector<Node*> users) {
+    vector<string> common_follower;
+    for (string followers1 : user1->follower) {
+        for (string followers2 : user2->follower) {
+            if (followers1 == followers2 ) {
+                common_follower.push_back(users[stoi(followers1)-1]->name);
+                //avoiding redundancy
+                sort(common_follower.begin(),common_follower.end());
+                common_follower.erase(unique(common_follower.begin(), common_follower.end()), common_follower.end());
+            }
+        }
+    }
+    return common_follower;
+}
+
+//for each user, suggest a list of users to follow (the followers of his followers) O(n^2)
+vector<string> suggested_followers(Node* user, vector<Node*> users) {
+    vector<string> suggested_follower;
+    for (string followers : user->follower) {
+        for (string followers2 : users[stoi(followers) -1]->follower) {
+            if (followers2 != user->id) {
+                suggested_follower.push_back(users[stoi(followers2)-1]->name);
+                //avoiding redundancy
+                sort(suggested_follower.begin(), suggested_follower.end());
+                suggested_follower.erase(unique(suggested_follower.begin(), suggested_follower.end()), suggested_follower.end());
+            }
+        }
+    }
+    return suggested_follower;
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 int main()
 {
@@ -125,7 +203,29 @@ int main()
     vector <string> tag = tags(xml_file);
     //call function tonodes to convert tags to nodes 
     vector<Node*> users = toNodes(tag);
-   
-    
+
+    //testing Network Analysis function
+    // 
+    //who is the most influencer user (has the most followers)
+    string influncer = Influncer(users);
+    cout << influncer<<endl;
+
+    //who is the most active user (connected to lots of users)
+    string active = Active(users);
+    cout << active << endl;
+
+    //the mutual followers between 2 users
+    vector<string> common = common_followers(users[1],users[2],users);
+    for (int i = 0; i < common.size(); i++) {
+        cout << common[i]<<",";
+    }
+    cout  << endl;
+
+    //for each user, suggest a list of users to follow
+    vector<string> suggested = suggested_followers(users[0], users);
+    for (int i = 0; i < suggested.size(); i++) {
+        cout << suggested[i] << "-";
+    }
+
     return 0;
 }
