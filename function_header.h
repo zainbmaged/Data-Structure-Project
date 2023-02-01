b7a17abeb8cfbc9bc8ce0f0561bf29d9ede7ef3f
zainbmaged <1901420@eng.asu.edu.eg>
2023-02-01 22:57:16 +0200

    Update and rename function_header.h to headers/function_header.h

--- function_header.h
+++
@@ -1,6 +0,0 @@
-
-vector <string> readxml(string name);
-string xml2json();
-void trim(string &s);
-void rtrim(string &s);
-void trim(string &s);
---
+++ headers/function_header.h
@@ -0,0 +1,4 @@
+string readxml(string name); // read xml and return it in single string
+vector <string> tags(string xmlstring); // save all tags and values in  vector tags
+vector <string> xmljson(vector <string> tag); // save json tags and value in  vector jsons
+string print_json(vector <string> jsons);// for reading json in string stream and return json in one string 
